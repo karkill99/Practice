b77ef8d1471cea1bae1850d14bb717dda7c090bc
karkill99 <kartikmm99@gmail.com>
2022-06-02 12:54:50 +0530

    Practic

---
+++ Javascript/Tests-May-Week-4/Question1/Question-1/script.js
@@ -0,0 +1,23 @@
+// var
+console.log("Example of Var");
+var a =856;
+var b=774;
+console.log(a+b);
+document.write("Example of var in consle <br>");
+document.write(a+b+'<br>');
+// let
+
+let c=67;
+let d=47;
+console.log("Example of let");
+console.log(c+d);
+document.write("Example of let in consle <br>");
+document.write(c+d+'<br>');
+
+// const
+const e=58;
+const f=97;
+console.log("Example of const");
+console.log(e+f);
+document.write("Example of constant in consle <br>");
+document.write(e+f+'<br>');
---
+++ Javascript/Tests-May-Week-4/Question1/Question-5/script.js
@@ -0,0 +1,27 @@
+
+let weekname = "5";
+switch (weekname) {
+    case "1":
+        console.log("The Day of Week is Sunday.");
+        break;
+    case "2":
+        console.log("The Day of Week is Monday.");
+        break;
+    case "3":
+        console.log("The Day of Week is Tuesday.");
+        break;
+    case "4":
+        console.log("The Day of Week is Wednesday.");
+        break;
+    case "5":
+        console.log("The Day of Week is Thursday.");
+        break;
+    case "6":
+        console.log("The Day of Week is Friday.");
+        break;
+    case "7":
+        console.log("The Day of Week is Saturday");
+        break;
+    default:
+        console.log("Invalid Input.");
+}
