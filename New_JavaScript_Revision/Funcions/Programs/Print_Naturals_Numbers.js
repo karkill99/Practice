d0600dd2b99f90a678406f16eef023f93b8965dd
karkill99 <kartikmm99@gmail.com>
2022-10-18 11:43:33 +0530

    Second

---
+++ New_JavaScript_Revision/Funcions/Programs/Print_Naturals_Numbers.js
@@ -0,0 +1,13 @@
+// WAP To Print Firs 10 Natural Numbers
+34
+
+// WAP To Print upto N Numbers.
+let x = prompt();
+
+function numbersUptoN(){
+    for(let i=1; i<x; i++){
+        console.log(i);
+    }
+}
+
+numbersUptoN();
