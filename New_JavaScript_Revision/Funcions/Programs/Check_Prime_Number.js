a9528e330add20993513ec10af3f5dea699f9c11
karkill99 <kartikmm99@gmail.com>
2022-10-29 19:27:21 +0530

    Second

--- New_JavaScript_Revision/Funcions/Programs/Check_Prime_Number.js
+++ New_JavaScript_Revision/Funcions/Programs/Check_Prime_Number.js
@@ -1,15 +1,20 @@
 // WAP A Program To Check A Number Is Prime Or Not
-
-let num=5;
-
+// let num = parseInt(prompt("Enter Any Number"));
+let num=1;
 function checkPrimeOrNot(){
+    // if(num<=1){
+    //     return false;
+    // }
     for(let i=2; i<num; i++){
-        if(i%num===0){ 
-        console.log("Num Is Not Prime");
-    }else {
-        console.log("Num Prime");
-    }
+        if(num%i===0){
+            return false;
+        }
+    }    
+    return true;
 }
+// checkPrimeOrNot();
+if(checkPrimeOrNot() === true){
+    console.log("Num Is Prime");
+} else {
+    console.log("Num Is Not Prime");
 }
-
-checkPrimeOrNot();
