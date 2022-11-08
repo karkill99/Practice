ce7f0150e0b2e84c5dcb51de0739372e1f8928e7
karkill99 <kartikmm99@gmail.com>
2022-11-08 14:53:20 +0530

    second

--- New_JavaScript_Revision/Funcions/Programs/Check_All_Prime_Upto_N.js
+++ New_JavaScript_Revision/Funcions/Programs/Check_All_Prime_Upto_N.js
@@ -1,24 +1,24 @@
 // WAP a program to print all prime numbers upto N
 
-// let n = parseInt(prompt("Enter Any Number"));
-// for(let i= 1; i<=n; i++){
-//     if(fun(i)){
-//         console.log(i);
-//     }
-// }
+let n = parseInt(prompt("Enter Any Number"));
+for(let i= 1; i<=n; i++){
+    if(fun(i)){
+        console.log(i);
+    }
+}
 
-// function fun(number){
-//     if(number<=1){
-//         return false;
-//     }
+function fun(number){
+    if(number<=1){
+        return false;
+    }
 
-//     for(let i=2; i<number/2; i++){
-//         if(number%i === 0){
-//             return false;
-//         }
-//     }
-//     return true;
-// }
+    for(let i=2; i<number/2; i++){
+        if(number%i === 0){
+            return false;
+        }
+    }
+    return true;
+}
 
 // **********************************************
 // Print all prime Numbers upto N
@@ -40,6 +40,6 @@ function fun(num){
             return false;
         }
     }
-    return true;
+    return true
 }
 
