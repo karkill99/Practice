57c8441e8120fe54c4e30fe99472da2261bdbca7
karkill99 <kartikmm99@gmail.com>
2022-11-15 14:38:59 +0530

    First

--- New_JavaScript_Revision/Funcions/Programs/Print_Fibonacci_Numbers_Till_N.js
+++ New_JavaScript_Revision/Funcions/Programs/Print_Fibonacci_Numbers_Till_N.js
@@ -1,15 +1,28 @@
 
-let num =20;
+// let num =20;
 
-function fun(){
-    let a = 0;
-    let b = 1;
-    let c;
-    for(let i = 2; i<num; i++){
-        c=a+b;
-        a=b;
+// function fun(num){
+//     let a = 0;
+//     let b = 1;
+//     let c;
+//     for(let i = 2; i<num; i++){
+//         a=b;
+//         b=c;
+//         c=a+b;
+//         console.log(a);
+//     }
+// }
+// fun(40);
+let Number
+function fun(Number){
+    let a=0;
+    let b=1;
+    let c=1;
+    for(let idx=0; idx<Number; idx++){
+        console.log(a);
+        a=b; 
         b=c;
-        console.log(c);
+        c=a+b;
     }
 }
-fun();
+fun(10);
