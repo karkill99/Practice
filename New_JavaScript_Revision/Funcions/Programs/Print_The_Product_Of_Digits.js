2f2a213fb1ec20414eda527d1a5234f358102e0f
karkill99 <kartikmm99@gmail.com>
2022-11-16 16:31:03 +0530

    First

---
+++ New_JavaScript_Revision/Funcions/Programs/Print_The_Product_Of_Digits.js
@@ -0,0 +1,10 @@
+// WAP Print_The_Sum_Of_Digits
+
+let number = 12346312;
+let sum = 1;
+while (number > 0) {
+  sum = sum * (number % 10);
+  number = parseInt(number / 10);
+}
+
+console.log(sum);
