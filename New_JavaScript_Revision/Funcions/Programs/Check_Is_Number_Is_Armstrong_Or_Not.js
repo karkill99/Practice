ac6de706d0c450d8d909812fba04e9c06927f070
karkill99 <kartikmm99@gmail.com>
2022-11-26 17:56:04 +0530

    First

--- New_JavaScript_Revision/Funcions/Programs/Check_Is_Number_Is_Armstrong_Or_Not.js
+++ New_JavaScript_Revision/Funcions/Programs/Check_Is_Number_Is_Armstrong_Or_Not.js
@@ -1,5 +1,20 @@
 let number = parseInt(prompt("Enter Any Number"));
+let arm = 0;
 
 function fun(num) {
-  temp = num % 10;
+  let org = num;
+  while (num > 0) {
+    temp = num % 10;
+    arm = arm + temp * temp * temp;
+
+    num = num / 10;
+  }
+  if (arm === org) {
+    console.log("Number Is Armsrong");
+  }
+
+  console.log();
 }
+
+fun(number);
+// console.log(fun(number));
