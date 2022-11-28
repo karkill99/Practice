3b6db34516d2e9c9a54ec713fabf75b4353efe87
karkill99 <kartikmm99@gmail.com>
2022-11-28 17:18:00 +0530

    Second

--- New_JavaScript_Revision/Funcions/Programs/Sum_Of_Qube_Of_Each_Digits.js
+++ New_JavaScript_Revision/Funcions/Programs/Sum_Of_Qube_Of_Each_Digits.js
@@ -4,14 +4,16 @@ let sum = 0;
 function fun(num) {
   while (num > 0) {
     let temp = num % 10;
-    // for (let idx = 1; idx <= temp; idx++) {
+
     sum = sum + temp * temp * temp;
+
+    // Another Method
+    // for (let idx = 1; idx <= temp; idx++) {
     // }
+
     num = parseInt(num / 10);
   }
   console.log(sum);
 }
 
 fun(number);
-
-1234;
