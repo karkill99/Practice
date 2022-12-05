028ec7b56fe3f84138a094b4fdf8648a52970da6
karkill99 <kartikmm99@gmail.com>
2022-12-05 17:13:07 +0530

    First

--- New_JavaScript_Revision/Funcions/Concepts/Arrays/SumOfAllElementsOfArray.js
+++ New_JavaScript_Revision/Funcions/Concepts/Arrays/SumOfAllElementsOfArray.js
@@ -1,23 +1,37 @@
 // Array Operations
-// push
-// pop
-// shift
-// unshift
+
+//1. push
+//2. pop
+//3. shift
+//4. unshift
 
 // push operation
-let arr = [1, 2, 3, 4, 5, 6, 7];
+// let arr = [1, 2, 3, 4, 5, 6, 7];
 
-function sum(param) {
-  let sum = 0;
+// function sum(param) {
+//   let sum = 0;
 
-  // Getting The Sum
-  for (let i = 0; i < param.length; i++) {
-    sum = sum + arr[i];
-  }
+//   // Getting The Sum
+//   for (let i = 0; i < param.length; i++) {
+//     sum = sum + arr[i];
+//   }
 
-  // Append The Sum
-  arr.push(sum);
-  return arr;
+//   // Append The Sum
+//   arr.push(sum);
+//   return arr;
+// }
+
+// console.log(sum(arr)); // Arguments
+
+// pop operation
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7];
+
+let res = popOperation(arr1);
+
+function popOperation(arr1) {
+  arr1.pop();
+  return arr1;
 }
 
-console.log(sum(arr)); // Arguments
+console.log(res);
