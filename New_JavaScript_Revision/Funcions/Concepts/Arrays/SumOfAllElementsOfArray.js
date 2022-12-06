105708f62ea74a29ebc20d427107a1339f909459
karkill99 <kartikmm99@gmail.com>
2022-12-06 19:50:01 +0530

    First

--- New_JavaScript_Revision/Funcions/Concepts/Arrays/SumOfAllElementsOfArray.js
+++ New_JavaScript_Revision/Funcions/Concepts/Arrays/SumOfAllElementsOfArray.js
@@ -25,13 +25,19 @@
 
 // pop operation
 
+// let arr1 = [1, 2, 3, 4, 5, 6, 7];
+
+// let res = popOperation(arr1);
+
+// function popOperation(arr1) {
+//   arr1.pop();
+//   return arr1;
+// }
+
+// console.log(res);
+
 let arr1 = [1, 2, 3, 4, 5, 6, 7];
 
-let res = popOperation(arr1);
-
-function popOperation(arr1) {
-  arr1.pop();
-  return arr1;
-}
-
-console.log(res);
+let l = arr1.length - 1;
+// console.log(l);
+console.log(arr1[parseInt(l / 2)]);
