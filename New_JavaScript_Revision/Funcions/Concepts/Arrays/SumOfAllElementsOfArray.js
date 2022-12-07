0d45d20efac241f5e406d13ac1f49954dcbcb550
karkill99 <kartikmm99@gmail.com>
2022-12-07 13:36:33 +0530

    First

--- New_JavaScript_Revision/Funcions/Concepts/Arrays/SumOfAllElementsOfArray.js
+++ New_JavaScript_Revision/Funcions/Concepts/Arrays/SumOfAllElementsOfArray.js
@@ -5,6 +5,7 @@
 //3. shift
 //4. unshift
 
+/************************************************/
 // push operation
 // let arr = [1, 2, 3, 4, 5, 6, 7];
 
@@ -20,9 +21,9 @@
 //   arr.push(sum);
 //   return arr;
 // }
-
 // console.log(sum(arr)); // Arguments
 
+/************************************************/
 // pop operation
 
 // let arr1 = [1, 2, 3, 4, 5, 6, 7];
@@ -36,8 +37,50 @@
 
 // console.log(res);
 
-let arr1 = [1, 2, 3, 4, 5, 6, 7];
+/************************************************/
+// Shift Operation
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-let l = arr1.length - 1;
-// console.log(l);
-console.log(arr1[parseInt(l / 2)]);
+// console.log(arr.length);
+// let res = shifting(arr);
+// function shifting(arr) {
+//   arr.shift();
+//   return arr;
+// }
+// console.log(res);
+// console.log(arr.length);
+
+/************************************************/
+// UnShift Operation
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// unShifting(arr);
+
+// function unShifting(arr) {
+//   arr.unshift(4);
+//   return arr;
+// }
+// console.log(arr);
+
+/************************************************/
+// Delete The Middle Element
+// let arr1 = [1, 2, 3, 4, 5, 6, 7];
+
+// let l = arr1.length;
+// let middle = parseInt((l - 1) / 2);
+
+// console.log("Deleted Element " + arr1[middle]);
+// for (let i = middle; i < l - 1; i++) {
+//   arr1[i] = arr1[i + 1];
+// }
+
+// arr1.pop();
+// console.log(arr1);
+
+/************************************************/
+// Delete The Middle Element Using Slice
+// let str = arr1.length / 2;
+// console.log(str);
+// arr1.splice(str, 1);
+// console.log(arr1);
